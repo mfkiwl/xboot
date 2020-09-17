937b2dfe92a75b5877a35fef002de7ab3b551d85
JianJunJiang <8192542@qq.com>
2020-09-17 22:03:33 +0800

    [gpreftools]using google gpreftools to profiling
    kill -s SIGUSR2 11533
    kill -s SIGUSR2 11533
    google-pprof --callgrind ./xboot xboot.prof > xboot.callgrind
    kcachegrind xboot.callgrind

--- src/arch/x64/mach-sandbox/libsandbox/sandbox.c
+++ src/arch/x64/mach-sandbox/libsandbox/sandbox.c
@@ -63,6 +63,26 @@ static size_t file_read_to_memory(const char * filename, char ** buffer)
 	return len;
 }
 
+static void set_gperf_status(int signum)
+{
+	static int flag = 0;
+	if(signum == SIGUSR2)
+	{
+		if(!flag)
+		{
+			flag = 1;
+			ProfilerStart("xboot.prof");
+			printf("profiler start\r\n");
+		}
+		else
+		{
+			flag = 0;
+			ProfilerStop();
+			printf("profiler stop\r\n");
+		}
+	}
+}
+
 void sandbox_init(int argc, char * argv[])
 {
 	size_t heap_size = 256 * 1024 * 1024;
@@ -168,6 +188,7 @@ void sandbox_init(int argc, char * argv[])
 	tcsetattr(0, TCSANOW, &ta);
 
 	SDL_Init(SDL_INIT_EVERYTHING);
+	signal(SIGUSR2, set_gperf_status);
 }
 
 void sandbox_exit(void)
